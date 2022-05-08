c6bafc682b6eb0bb57cb06c5d62e4dd4a00f9bd9
Alexander Naumow <anaumow@stud.hs-heilbronn.de>
2022-05-08 04:52:42 +0200

    checkstyle

--- DevOps.java
+++ DevOps.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList.*;
+
+import java.util.List.*;
 public class DevOps {
+  public static void main(String[] args) {
+    System.out.print("This line is correct");
+                  System.out.print("Wrong position");
+                  int empty;
+
+  }
 }
